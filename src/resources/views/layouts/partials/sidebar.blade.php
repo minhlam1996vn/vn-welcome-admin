f3d49df3bc12e42cc3b21e25472e5fe78aa8c13e
NTA_LamLDM <lamldm.nta@gmail.com>
2024-01-15 16:51:30 +0700

    update icon

--- src/resources/views/layouts/partials/sidebar.blade.php
+++ src/resources/views/layouts/partials/sidebar.blade.php
@@ -9,28 +9,28 @@
 
             <li class="sidebar-item">
                 <a class='sidebar-link' href='{{ route('admin.dashboard') }}'>
-                    <i class="align-middle" data-feather="sliders"></i>
+                    <i class="align-middle" data-feather="home"></i>
                     <span class="align-middle">Bảng điều khiển</span>
                 </a>
             </li>
 
             <li class="sidebar-item">
                 <a class='sidebar-link' href='/pages-profile'>
-                    <i class="align-middle" data-feather="user"></i>
+                    <i class="align-middle" data-feather="sliders"></i>
                     <span class="align-middle">Quản lý danh mục</span>
                 </a>
             </li>
 
             <li class="sidebar-item">
                 <a class='sidebar-link' href='/pages-sign-in'>
-                    <i class="align-middle" data-feather="log-in"></i>
+                    <i class="align-middle" data-feather="book"></i>
                     <span class="align-middle">Quản lý bài viết</span>
                 </a>
             </li>
 
             <li class="sidebar-item">
                 <a class='sidebar-link' href='/pages-sign-in'>
-                    <i class="align-middle" data-feather="log-in"></i>
+                    <i class="align-middle" data-feather="user"></i>
                     <span class="align-middle">Cài đặt tài khoản</span>
                 </a>
             </li>
