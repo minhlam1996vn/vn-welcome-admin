152be19c4747993927a176ed515acd4455b65460
NTA_LamLDM <lamldm.nta@gmail.com>
2024-02-06 16:47:15 +0700

    update alert toast

--- src/resources/views/components/alert.blade.php
+++ src/resources/views/components/alert.blade.php
@@ -1,8 +1,15 @@
 @if ($message)
-    <div class="mb-4 alert alert-{{ $type }} alert-dismissible" role="alert">
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        <div class="alert-message">
-            {{ $message }}
+    <div class="toast-container position-fixed top-0 end-0 p-3">
+        <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="align-items-center text-bg-{{ $type }} border-0" role="alert">
+                <div class="d-flex">
+                    <div class="toast-body text-white">
+                        {{ $message }}
+                    </div>
+                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+                        aria-label="Close"></button>
+                </div>
+            </div>
         </div>
     </div>
 @endif
