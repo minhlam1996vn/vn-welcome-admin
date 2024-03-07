b3c653ccc26630e5e9a41abfbd6364fbd282f450
NTA_LamLDM <lamldm.nta@gmail.com>
2024-03-07 12:00:43 +0700

    update message valid

--- src/app/Http/Requests/Admin/CategoryRequest.php
+++ src/app/Http/Requests/Admin/CategoryRequest.php
@@ -33,4 +33,16 @@ class CategoryRequest extends FormRequest
             'category_name' => ['required', $uniqueRule, 'max:50'],
         ];
     }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'category_name' => 'tên danh mục',
+        ];
+    }
 }
